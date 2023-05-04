a7c9855558fc5e8707e73559671b86a1915ca004
Chima-Expert <chima@asktoexpert.com>
2023-05-04 16:38:16 +0100

    Updated webhook checkout route path, still for testing

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 
 // Must come before express.json()
 app.post(
-  '/stripe-webhook',
+  'stripe-webhook-checkout',
   express.raw({ type: 'application/json' }),
   businessController.stripePaymentWebhookHandler
 );
