5c89866a3c085fb90a3ad7907ececc8252690abb
Chima-Expert <chima@asktoexpert.com>
2023-05-09 11:34:40 +0100

    Did the update/edit city api including the photo upload and syncing with stripe price

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -7,7 +7,7 @@ const { toTitleCase } = require('../utils/string-utils');
 exports.getAllCities = async (req, res) => {
   try {
     const cities = await City.find({}).select('name imgUrl isFeatured searchesCount');
-    res.status(200).json({ status: 'SUCCESS', results: cities.length, cities });
+    res.status(200).json({ results: cities.length, cities });
   } catch (err) {
     console.log('Error: ', err);
 
@@ -47,7 +47,7 @@ exports.getAllCities = async (req, res) => {
 //     ]);
 
 //     if (!result?.cities) {
-//       return res.json({ status: 'SUCCESS', source: 'db', results: 0, cities: [] });
+//       return res.json({  source: 'db', results: 0, cities: [] });
 //     }
 
 //     // Cache matching cities
@@ -59,7 +59,7 @@ exports.getAllCities = async (req, res) => {
 //       await cityQueries.cacheCitySearchResults(cities);
 //     }
 
-//     res.status(200).json({ status: 'SUCCESS', source: 'db', results: cities.length, cities });
+//     res.status(200).json({  source: 'db', results: cities.length, cities });
 //   } catch (err) {
 //     console.log('Error: ', err);
 
@@ -95,11 +95,11 @@ exports.searchCities = async (req, res, next) => {
 
 exports.toggleCityFeatured = async (req, res) => {
   try {
-    const city = await City.findById(req.params.cityId).select('isFeatured');
-    city.isFeatured = city.isFeatured;
+    const city = await City.findById(req.params.id);
+    city.isFeatured = !city.isFeatured;
 
-    await city.save({ validateBeforeSave: false });
-    res.status(200).json({ status: 'SUCCESS', city });
+    await city.save();
+    res.status(200).json({ city });
   } catch (err) {
     console.log(err);
     res.status(400).json({ error: err.message });
@@ -109,9 +109,88 @@ exports.toggleCityFeatured = async (req, res) => {
 exports.getStateNames = async (req, res) => {
   try {
     const stateNames = await City.find({}).select('stateName').distinct('stateName');
-    res.status(200).json({ status: 'SUCCESS', results: stateNames.length, stateNames });
+    res.status(200).json({ results: stateNames.length, stateNames });
   } catch (err) {
     console.log(err);
     res.status(400).json({ error: err.message });
+  }
+};
+
+exports.resizeCityPhoto = async (req, res, next) => {
+  try {
+    // Validate uploaded photo
+    if (!req.file.mimeType.endsWith('image'))
+      return res.status(400).json({
+        msg: `Uploaded file of type ${req.file.mimeType} is not an image`,
+      });
+
+    // Store image in filesystem
+    const filePath = `public/img/cities/city-${req.params.cityId}-${Date.now()}.jpeg`;
+
+    // Resize photo
+    const sharpResult = await sharp(req.file.buffer)
+      .resize(2000, 1333)
+      .jpeg({ quality: 90 })
+      .toFormat('jpeg');
+
+    // Delete file from our server
+    fs.unlink(filePath, err => {
+      console.log(
+        err ? 'Could not delete city photo from server' : 'City photo deleted successfully'
+      );
+    });
+    res.json({ status: 'success' });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.updateCity = async (req, res) => {
+  try {
+    const city = await City.findById(req.params.id);
+
+    if (req.body.price) {
+      const stripePrices = await stripe.prices.list();
+      const existingStripePrice = stripePrices.data.find(pr => {
+        return +pr.unit_amount_decimal / 100 === +req.body.price;
+      });
+
+      if (existingStripePrice) {
+        const cityClaimProduct = stripePrices.data.find(price =>
+          price.product.name.toLowerCase().includes('city claim')
+        );
+
+        // Create a new price
+        const newPrice = await stripe.prices.create({
+          unit_amount: req.body.price,
+          currency: 'usd',
+          recurring: { interval: 'month' },
+          product: cityClaimProduct.id,
+        });
+
+        city.price = {
+          amount: req.body.price,
+          currency: newPrice.currency,
+        };
+      } else {
+        city.price = {
+          amount: +req.body.price,
+          currency: existingStripePrice.currency,
+          stripePriceId: existingStripePrice.id,
+          stripePriceNickname: existingStripePrice.nickname,
+        };
+      }
+      await city.save();
+    }
+
+    const updatedCity = await City.findByIdAndUpdate(city._id, req.body);
+    res.status(200).json({
+      msg: `${city.name} has been updated successfully`,
+      city: updatedCity,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ status: '400', error: err.message });
   }
 };
